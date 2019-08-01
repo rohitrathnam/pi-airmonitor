f72bd594ca1b658b756315ac0f0017d7bb210004
rohitrathnam <rohitrathnam.n@gmail.com>
2019-08-01 12:28:02 +0530

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -2,10 +2,6 @@
 #Install script for dependencies (works for raspberry pi3 ONLY)
 #execute: sudo ./setup.sh
 
-echo "Enabling SPI and I2C"
-raspi-config nonint do_spi %d
-raspi-config nonint do_i2c %d
-
 echo "Installing packages for math"
 apt-get update
 apt-get install libatlas-base-dev liblapack-dev libblas-dev
@@ -19,10 +15,9 @@ echo "Installing python libraries"
 cd deps
 pip3 install numpy-1.16.4-cp35-cp35m-linux_armv7l.whl
 pip3 install pandas-0.24.1-cp35-cp35m-linux_armv7l.whl
-pip3 install scikit_learn-0.20.2-cp35-cp35m-linux_armv7l.whl
-pip3 install xgboost-0.81-cp35-cp35m-linux_armv7l.whl
 pip3 install flask
 
 echo "Setup successful"
 echo "Run src/main.py to log the data in src/web/values.db"
 echo "Run src/web/web.py to start the server"
+echo "Follow https://www.raspberrypi.org/forums/viewtopic.php?p=1007463#p1007463"
