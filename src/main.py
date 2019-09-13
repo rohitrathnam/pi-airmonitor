5410fb51253aa4843bec4f64c88bf9525def3a72
rohitrathnam <rohitrathnam.n@gmail.com>
2019-09-13 18:04:40 +0530

    webserver changes

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-import Adafruit_MCP4725
+import MCP4725
 from Adafruit_BME280 import *
 from time import sleep
 import spidev
@@ -8,6 +8,10 @@ import pigpio
 import adxl345
 import honeywell_hpma115s0 as pmsensor
 import ads1115
+
+from flask import Flask, render_template, request
+app = Flask(__name__)
+import threading
 
 MUX0 = 5
 MUX1 = 6
@@ -24,8 +28,8 @@ spi.open(0, 1)
 spi.max_speed_hz = 100000
 spi.mode = 0b00
 
-hv = Adafruit_MCP4725.MCP4725(address=0x60, busnum=1)
-sv = Adafruit_MCP4725.MCP4725(address=0x61, busnum=1)
+hv = MCP4725.MCP4725(address=0x60, busnum=1)
+sv = MCP4725.MCP4725(address=0x61, busnum=1)
 bme = BME280(t_mode=BME280_OSAMPLE_8, p_mode=BME280_OSAMPLE_8, h_mode=BME280_OSAMPLE_8)
 pm = pmsensor.Honeywell()
 adxl = adxl345.ADXL345()
@@ -97,8 +101,8 @@ def read_bme():
 def log_db(gas, gas_raw, mux, temp, hum, pm25, pm10, acc_x, acc_y, acc_z, ldr):
 	with sql.connect("log.db") as con:
 		cur = con.cursor()
-		cur.execute("CREATE TABLE IF NOT EXISTS log (id INTEGER PRIMARY KEY AUTOINCREMENT, time TIMESTAMP, gas REAL, gas_raw REAL, mux INTEGER, temp REAL, hum REAL, pm25 INTEGER, pm10 INTEGER, acc_x REAL, acc_y REAL, acc_z REAL, ldr INTEGER)")
-		cur.execute("INSERT INTO log (gas, gas_raw, mux, temp, hum, pm25, pm10, acc_x, acc_y, acc_z, ldr) VALUES (?,?,?,?,?,?,?,?,?,?,?)", (gas, gas_raw, mux, temp, hum, pm2.5, pm10, acc_x, acc_y, acc_z, ldr))
+		cur.execute("CREATE TABLE IF NOT EXISTS log (id INTEGER PRIMARY KEY AUTOINCREMENT, time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, gas REAL, gas_raw REAL, mux INTEGER, temp REAL, hum REAL, pm25 INTEGER, pm10 INTEGER, acc_x REAL, acc_y REAL, acc_z REAL, ldr INTEGER)")
+		cur.execute("INSERT INTO log (gas, gas_raw, mux, temp, hum, pm25, pm10, acc_x, acc_y, acc_z, ldr) VALUES (?,?,?,?,?,?,?,?,?,?,?)", (gas, gas_raw, mux, temp, hum, pm25, pm10, acc_x, acc_y, acc_z, ldr))
 		con.commit()
 
 def set_mux(config):
@@ -115,32 +119,84 @@ def set_mux(config):
 		pi.write(MUX0,1)
 		pi.write(MUX1,1)
 
-set_mux(mux)
-set_sv(4.3)
-set_hv(0)
+@app.route('/')
+def home():
+	con = sql.connect("log.db")
 
-try:
-	while True:
-		[raw_adc, sensor_cur] = read_adc()
-		[temp, hum] = read_bme()
-		if(raw_adc>3.1 and mux>0):
-			mux = mux-1
-			set_mux(mux)
-		elif(raw_adc<1 and mux<3):
-			mux = mux+1
-			set_mux(mux)
-		print("sensor ", raw_adc, sensor_cur, mux)
-		print("bme ", temp, hum)
-		pm_data = pm.read()
-		print("pm sensor ", pm_data.pm10, pm_data.pm25)
-		acc = adxl.getAxes(True)
-		print("acc ", acc['x'], acc['y'], acc['z'])
-		ldr = ads.readADCSingleEnded()
-		ldr = int(ldr)
-		print("ldr ", ldr)
-		log_db(sensor_cur, raw_adc, mux, temp, hum, pm_data.pm25, pm_data.pm10, acc['x'], acc['y'], acc['z'], ldr)
+	with con:
+		cur = con.execute("SELECT * FROM log ORDER BY id DESC LIMIT 1")
+	for row in cur:
+		print("served ", row)
+	return render_template('home.html', rows=row)
 
-except KeyboardInterrupt:
-	print("exit")
-	spi.close()
+@app.route('/status')
+def status():
+	con = sql.connect("database.db")
+	cur = con.execute("select * from settings")
+	for row in cur:
+		print(row)
+	con.close()
+	return render_template("status.html",rows = row)
+
+@app.route('/settings')
+def settings():
+	return render_template("form.html")
+
+
+@app.route('/change',methods = ['POST', 'GET'])
+def change():
+	if request.method == 'POST':
+		try:
+			sv = request.form['sv']
+			hv = request.form['hv']
+			state = request.form['state']
+			with sql.connect("database.db") as con:
+				cur = con.cursor()
+				cur.execute("UPDATE settings SET state=?, hv=?, sv=? WHERE id=1",(state,hv,sv)) 
+				con.commit()
+			msg = "Success"
+		except:
+			con.rollback()
+			msg = "Error"
+
+		finally:
+			return render_template("result.html",msg = msg)
+			con.close()
+
+def webserver():
+	print("Flask init success")
+	app.run(host='0.0.0.0', port=80, debug=False)
+
+if __name__ == '__main__':
+	web = threading.Thread(target=webserver)
+	web.daemon = True
+	web.start()
+	set_mux(mux)
+	set_sv(4.3)
+	set_hv(0)
+
+	try:
+		while True:
+			[raw_adc, sensor_cur] = read_adc()
+			[temp, hum] = read_bme()
+			if(raw_adc>3.1 and mux>0):
+				mux = mux-1
+				set_mux(mux)
+			elif(raw_adc<1 and mux<3):
+				mux = mux+1
+				set_mux(mux)
+			print("sensor ", raw_adc, sensor_cur, mux)
+			print("bme ", temp, hum)
+			pm_data = pm.read()
+			print("pm sensor ", pm_data.pm10, pm_data.pm25)
+			acc = adxl.getAxes(True)
+			print("acc ", acc['x'], acc['y'], acc['z'])
+			ldr = ads.readADCSingleEnded()
+			ldr = int(ldr)
+			print("ldr ", ldr)
+			log_db(sensor_cur, raw_adc, mux, temp, hum, pm_data.pm25, pm_data.pm10, acc['x'], acc['y'], acc['z'], ldr)
+
+	except KeyboardInterrupt:
+		print("exit")
+		spi.close()
 
---
+++ src/test/honeywell_hpma115s0/__init__.py
@@ -0,0 +1,1 @@
+from .honeywell_hpma115s0 import HoneywellReading, Honeywell, HoneywellException
